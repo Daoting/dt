6a764d14984319059f4ca308b407a2b39b6d8bcc
daoting <daoting@outlook.com>
2021-05-25 13:50:10 +0800

    增加xaml和cs编辑器的上下文菜单

---
+++ Tools/Dt/DtCmds.cs
@@ -0,0 +1,120 @@
+﻿using Dt.Forms;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using Microsoft.VisualStudio.Text;
+using System;
+using System.ComponentModel.Composition;
+using System.ComponentModel.Design;
+using System.Globalization;
+using System.Threading;
+using System.Threading.Tasks;
+using Task = System.Threading.Tasks.Task;
+using Window = System.Windows;
+
+namespace Dt
+{
+    /// <summary>
+    /// Command handler
+    /// </summary>
+    internal sealed class DtCmds
+    {
+        const int LvCommandId = 0x0100;
+        const int FvCommandId = 0x0101;
+        const int LvCellExClsCmdId = 0x2000;
+
+        /// <summary>
+        /// Command menu group (command set GUID).
+        /// </summary>
+        public static readonly Guid CommandSet = new Guid("6ef30193-3795-49e0-b034-e320355b74ae");
+
+        /// <summary>
+        /// VS Package that provides this command, not null.
+        /// </summary>
+        private readonly AsyncPackage package;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DtCmds"/> class.
+        /// Adds our command handlers for menu (commands must exist in the command table file)
+        /// </summary>
+        /// <param name="package">Owner package, not null.</param>
+        /// <param name="commandService">Command service to add command to, not null.</param>
+        private DtCmds(AsyncPackage package, OleMenuCommandService commandService)
+        {
+            this.package = package ?? throw new ArgumentNullException(nameof(package));
+            commandService = commandService ?? throw new ArgumentNullException(nameof(commandService));
+
+            commandService.AddCommand(new MenuCommand(InsertLv, new CommandID(CommandSet, LvCommandId)));
+            commandService.AddCommand(new MenuCommand(InsertFv, new CommandID(CommandSet, FvCommandId)));
+            commandService.AddCommand(new MenuCommand(AddLvCellExCls, new CommandID(CommandSet, LvCellExClsCmdId)));
+        }
+
+        /// <summary>
+        /// Gets the instance of the command.
+        /// </summary>
+        public static DtCmds Instance
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// Gets the service provider from the owner package.
+        /// </summary>
+        private Microsoft.VisualStudio.Shell.IAsyncServiceProvider ServiceProvider
+        {
+            get
+            {
+                return this.package;
+            }
+        }
+
+        /// <summary>
+        /// Initializes the singleton instance of the command.
+        /// </summary>
+        /// <param name="package">Owner package, not null.</param>
+        public static async Task InitializeAsync(AsyncPackage package)
+        {
+            // Switch to the main thread - the call to AddCommand in LvCmd's constructor requires
+            // the UI thread.
+            await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync(package.DisposalToken);
+
+            OleMenuCommandService commandService = await package.GetServiceAsync(typeof(IMenuCommandService)) as OleMenuCommandService;
+            Instance = new DtCmds(package, commandService);
+        }
+
+        /// <summary>
+        /// This function is the callback used to execute the command when the menu item is clicked.
+        /// See the constructor to see how the menu item is associated with this function using
+        /// OleMenuCommandService service and MenuCommand class.
+        /// </summary>
+        /// <param name="sender">Event sender.</param>
+        /// <param name="e">Event args.</param>
+        void InsertLv(object sender, EventArgs e)
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+            string message = string.Format(CultureInfo.CurrentCulture, "Inside {0}.MenuItemCallback()", this.GetType().FullName);
+            string title = "LvCmd";
+
+            // Show a message box to prove we were here
+            VsShellUtilities.ShowMessageBox(
+                this.package,
+                message,
+                title,
+                OLEMSGICON.OLEMSGICON_INFO,
+                OLEMSGBUTTON.OLEMSGBUTTON_OK,
+                OLEMSGDEFBUTTON.OLEMSGDEFBUTTON_FIRST);
+        }
+
+
+        void InsertFv(object sender, EventArgs e)
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+            new FvForm(this).ShowDialog();
+        }
+
+        void AddLvCellExCls(object sender, EventArgs e)
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+        }
+    }
+}
--- Tools/Dt/DtPackage.cs
+++ Tools/Dt/DtPackage.cs
@@ -25,6 +25,7 @@ namespace Dt
     /// </remarks>
     [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
     [Guid(DtPackage.PackageGuidString)]
+    [ProvideMenuResource("Menus.ctmenu", 1)]
     public sealed class DtPackage : AsyncPackage
     {
         /// <summary>
@@ -46,6 +47,7 @@ namespace Dt
             // When initialized asynchronously, the current thread may be a background thread at this point.
             // Do any initialization that requires the UI thread after switching to the UI thread.
             await this.JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
+            await DtCmds.InitializeAsync(this);
         }
 
         #endregion
