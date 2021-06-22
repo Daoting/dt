76bb773f9fe326a5a6a3de0e36ca61c7ce6aab1e
daoting <daoting@outlook.com>
2021-06-22 15:42:16 +0800

    wasm 的 ServerStreamRpc 完美收工

--- Service/src/Dt.Core/Rpc/Client/Rpc/ServerStreamRpc.cs
+++ Service/src/Dt.Core/Rpc/Client/Rpc/ServerStreamRpc.cs
@@ -43,11 +43,26 @@ namespace Dt.Core.Rpc
                 using (var request = CreateRequestMessage())
                 using (var content = new PushStreamContent((ws) => RpcClientKit.WriteFrame(ws, _data, _isCompressed)))
                 {
+                    // 必须设置 WebAssemblyEnableStreamingResponse 为 true，WASM 才会返回 stream 类型的 response；
+                    // 如果不设置，则只在整个response结束时收到一次，无法实现服务器推送流模式！！！参见：
+                    // https://github.com/grpc/grpc-dotnet/blob/master/src/Grpc.Net.Client.Web/GrpcWebHandler.cs#L137
+                    //
+                    // 此限制和服务端无关，只是WASM客户端在收到服务器推送后的处理方式不同，默认response 非 stream模式，参见：
+                    // https://github.com/mono/mono/blob/a0d69a4e876834412ba676f544d447ec331e7c01/sdks/wasm/framework/src/System.Net.Http.WebAssemblyHttpHandler/WebAssemblyHttpHandler.cs#L149
+                    //
+                    // https://github.com/mono/mono/issues/18718
+#if WASM
+#pragma warning disable CS0618
+                    request.Properties["WebAssemblyEnableStreamingResponse"] = true;
+#pragma warning restore CS0618
+#endif
+
                     request.Content = content;
                     // 一定是ResponseHeadersRead，否则只在结束时收到一次！！！众里寻他千百度
                     var response = await _client.SendAsync(request, HttpCompletionOption.ResponseHeadersRead, CancellationToken.None);
                     response.EnsureSuccessStatusCode();
-                    // wasm 此方法异常！
+
+                    // wasm 增加上述设置后返回 stream 正常！
                     responseStream = await response.Content.ReadAsStreamAsync();
                 }
             }
