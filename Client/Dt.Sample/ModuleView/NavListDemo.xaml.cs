e81654065bb2730a5c01ee2c64760d35df9955de
daoting <daoting@outlook.com>
2021-10-20 16:12:02 +0800

    NavList样式调整

--- Client/Dt.Sample/ModuleView/NavListDemo.xaml.cs
+++ Client/Dt.Sample/ModuleView/NavListDemo.xaml.cs
@@ -32,10 +32,10 @@ namespace Dt.Sample
         {
             _nav.Data = new Nl<Nav>
             {
-                new Nav("基础视图", typeof(LvViewBase), Icons.汉堡) { Desc = "三类视图，两种数据源，三种选择模式，支持分组三类视图，两种数据源，三种选择模式，支持分组", Warning = "12" },
-                new Nav("表格视图", typeof(LvTable), Icons.分组),
-                new Nav("列表视图", typeof(LvList), Icons.全选) { Desc = "只垂直滚动" },
-                new Nav("磁贴视图", typeof(LvTile), Icons.日历) { Desc = "平铺式磁贴，一行多格，只垂直滚动" },
+                new Nav("空白窗口", typeof(BlankWin), Icons.修改) { Desc = "行/项高度默认NaN，表示自动高度，因NavList的数据行数较少，每项的内容多少不同！设置0时表示以第一项高度为准，大于0时表示固定行/项高度。", Warning = "12" },
+                new Nav("主区窗口", typeof(SingleViewWin), Icons.打印) { Desc = "有标题栏的空白窗口" },
+                new Nav("对话框", typeof(DlgDemo), Icons.日历) { Desc = "模拟传统对话框" },
+                new Nav("查找图标", typeof(IconDemo), Icons.图片) { Desc = "内置的矢量文字，可用作图标、提示" },
             };
         }
 
@@ -43,10 +43,10 @@ namespace Dt.Sample
         {
             _nav.Data = new Nl<Nav>
             {
-                new Nav("表格视图", typeof(LvTable), Icons.分组),
-                new Nav("基础视图", typeof(LvViewBase), Icons.汉堡) { Warning = "5" },
-                new Nav("列表视图", typeof(LvList), Icons.全选),
-                new Nav("磁贴视图", typeof(LvTile), Icons.日历),
+                new Nav("主区窗口", typeof(SingleViewWin), Icons.打印),
+                new Nav("对话框", typeof(DlgDemo), Icons.日历) { Warning = "5" },
+                new Nav("查找图标", typeof(IconDemo), Icons.图片),
+                new Nav("空白窗口", typeof(BlankWin), Icons.修改),
             };
         }
 
@@ -54,10 +54,10 @@ namespace Dt.Sample
         {
             _nav.Data = new Nl<Nav>
             {
-                new Nav("基础视图", typeof(LvViewBase)) { Desc = "三类视图，两种数据源，三种选择模式，支持分组三类视图，两种数据源，三种选择模式，支持分组", Warning = "12" },
-                new Nav("表格视图", typeof(LvTable)),
-                new Nav("列表视图", typeof(LvList)) { Desc = "只垂直滚动" },
-                new Nav("磁贴视图", typeof(LvTile)) { Desc = "平铺式磁贴，一行多格，只垂直滚动" },
+                new Nav("查找图标", typeof(IconDemo)) { Desc = "内置的矢量文字，可用作图标、提示" },
+                new Nav("空白窗口", typeof(BlankWin)) { Desc = "行/项高度默认NaN，表示自动高度，因NavList的数据行数较少，每项的内容多少不同！设置0时表示以第一项高度为准，大于0时表示固定行/项高度。" },
+                new Nav("主区窗口", typeof(SingleViewWin)) { Desc = "有标题栏的空白窗口" },
+                new Nav("对话框", typeof(DlgDemo)) { Warning = "2", Desc = "模拟传统对话框" },
             };
         }
 
@@ -65,10 +65,10 @@ namespace Dt.Sample
         {
             _nav.Data = new Nl<Nav>
             {
-                new Nav("列表视图", typeof(LvList)),
-                new Nav("基础视图", typeof(LvViewBase)) { Warning = "1" },
-                new Nav("表格视图", typeof(LvTable)),
-                new Nav("磁贴视图", typeof(LvTile)),
+                new Nav("对话框", typeof(DlgDemo)) { Warning = "1" },
+                new Nav("主区窗口", typeof(SingleViewWin)),
+                new Nav("查找图标", typeof(IconDemo)),
+                new Nav("空白窗口", typeof(BlankWin)),
             };
         }
 
@@ -76,10 +76,10 @@ namespace Dt.Sample
         {
             _nav.Data = new Nl<Nav>
             {
-                new Nav("基础视图", typeof(LvViewBase), Icons.汉堡) { Desc = "三类视图，两种数据源，三种选择模式，支持分组三类视图，两种数据源，三种选择模式，支持分组", Warning = "12" },
-                new Nav("表格视图", typeof(LvTable), Icons.分组),
-                new Nav("列表视图", typeof(LvList)) { Desc = "只垂直滚动" },
-                new Nav("磁贴视图", typeof(LvTile)),
+                new Nav("查找图标", typeof(IconDemo)),
+                new Nav("主区窗口", typeof(SingleViewWin)) { Desc = "有标题栏的空白窗口" },
+                new Nav("对话框", typeof(DlgDemo), Icons.日历),
+                new Nav("空白窗口", typeof(BlankWin), Icons.修改) { Desc = "行/项高度默认NaN，表示自动高度，因NavList的数据行数较少，每项的内容多少不同！设置0时表示以第一项高度为准，大于0时表示固定行/项高度。", Warning = "12" },
             };
         }
 
