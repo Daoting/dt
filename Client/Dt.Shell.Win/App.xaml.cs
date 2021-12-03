bfb8cae63ada5e10f64a4d79f0566fb384471e0d
daoting <daoting@outlook.com>
2021-12-03 14:40:25 +0800

    不使用XamlControlsResources样式

--- Client/Dt.Shell.Win/App.xaml.cs
+++ Client/Dt.Shell.Win/App.xaml.cs
@@ -1,59 +1,29 @@
-﻿using Dt.Base;
-using Dt.Shell.Win;
-using Microsoft.UI.Xaml;
-using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
-using Microsoft.UI.Xaml.Shapes;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.ApplicationModel;
-using Windows.ApplicationModel.Activation;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
+﻿#region 文件描述
+/******************************************************************************
+* 创建: Daoting
+* 摘要: 
+* 日志: 2021-12-03
+******************************************************************************/
+#endregion
 
-// To learn more about WinUI, the WinUI project structure,
-// and more about our project templates, see: http://aka.ms/winui-project-info.
+#region 引用命名
+using Dt.Base;
+using Microsoft.UI.Xaml;
+#endregion
 
 namespace Dt.Shell
 {
-    /// <summary>
-    /// Provides application-specific behavior to supplement the default Application class.
-    /// </summary>
     public partial class App : Application
     {
-        /// <summary>
-        /// Initializes the singleton application object.  This is the first line of authored code
-        /// executed, and as such is the logical equivalent of main() or WinMain().
-        /// </summary>
         public App()
         {
             InitializeComponent();
         }
 
-        /// <summary>
-        /// Invoked when the application is launched normally by the end user.  Other entry points
-        /// will be used such as when the application is launched to open a specific file.
-        /// </summary>
-        /// <param name="args">Details about the launch request and process.</param>
-        protected override void OnLaunched(Microsoft.UI.Xaml.LaunchActivatedEventArgs p_args)
+        protected override void OnLaunched(LaunchActivatedEventArgs p_args)
         {
             _ = Startup.Launch<Stub>(p_args.Arguments, null);
             //new MainWindow().Activate();
-        }
-
-        private Window m_window;
-
-        private void OnActivated(object sender, WindowActivatedEventArgs args)
-        {
-            //var win = Window.Current;
-            //_ = Startup.Launch<Stub>(null, null);
         }
     }
 }
