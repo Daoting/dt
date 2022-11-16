9f0fdbc0e434f4c00665210630a2dbd37b97ee21
daoting <daoting@outlook.com>
2022-11-16 08:44:10 +0800

    Env数据源辅助属性

--- Client/Dt.Base/ListView/CellUI/Env.cs
+++ Client/Dt.Base/ListView/CellUI/Env.cs
@@ -20,30 +20,20 @@ namespace Dt.Base
     /// </summary>
     public class Env
     {
+        #region 成员变量
         ViewItem _item;
         Dot _dot;
+        #endregion
 
+        #region 构造方法
         internal Env(ViewItem p_item, Dot p_dot)
         {
             _item = p_item;
             _dot = p_dot;
         }
+        #endregion
 
-        /// <summary>
-        /// 获取行的数据源
-        /// </summary>
-        public object Data => _item.Data;
-
-        /// <summary>
-        /// 获取Row数据源
-        /// </summary>
-        public Row Row => _item.Row;
-
-        /// <summary>
-        /// 获取当前单元格的值
-        /// </summary>
-        public object CellVal => _item[_dot.ID];
-
+        #region UI
         /// <summary>
         /// 自定义单元格UI
         /// </summary>
@@ -140,7 +130,123 @@ namespace Dt.Base
         {
             return _item.CreateDefaultUI(_dot);
         }
+        #endregion
 
+        #region 数据源
+        /// <summary>
+        /// 获取行的数据源
+        /// </summary>
+        public object Data => _item.Data;
+
+        /// <summary>
+        /// 获取Row数据源
+        /// </summary>
+        public Row Row => _item.Row;
+
+        /// <summary>
+        /// 获取当前单元格的值
+        /// </summary>
+        public object CellVal => _item[_dot.ID];
+
+        /// <summary>
+        /// 根据当前单元格的字符串值，为null时返回string.Empty！！！
+        /// </summary>
+        /// <returns>字符串值</returns>
+        public string Str => GetVal<string>(_dot.ID);
+
+        /// <summary>
+        /// 当前单元格的值是否为1或true
+        /// </summary>
+        /// <returns>true 表示列值为空</returns>
+        public bool Bool => GetVal<bool>(_dot.ID);
+
+        /// <summary>
+        /// 当前单元格的double值，为null时返回零即default(double)！！！
+        /// </summary>
+        /// <returns>double值</returns>
+        public double Double => GetVal<double>(_dot.ID);
+
+        /// <summary>
+        /// 当前单元格的整数值，为null时返回零即default(int)！！！
+        /// </summary>
+        /// <returns>整数值</returns>
+        public int Int => GetVal<int>(_dot.ID);
+
+        /// <summary>
+        /// 当前单元格的64位整数值，为null时返回零即default(long)！！！
+        /// </summary>
+        /// <returns>整数值</returns>
+        public long Long => GetVal<long>(_dot.ID);
+
+        /// <summary>
+        /// 当前单元格的日期值，为null时返回DateTime.MinValue，即default(DateTime)！！！
+        /// </summary>
+        /// <returns>日期值</returns>
+        public DateTime Date => GetVal<DateTime>(_dot.ID);
+
+        /// <summary>
+        /// 获取当前行某列的值
+        /// </summary>
+        /// <typeparam name="T">将值转换为指定的类型</typeparam>
+        /// <param name="p_id">列名</param>
+        /// <returns>指定类型的值</returns>
+        public T GetVal<T>(string p_id)
+        {
+            object val = _item[p_id];
+            Type tgtType = typeof(T);
+
+            // null时
+            if (val == null)
+            {
+                // 字符串返回Empty！！！
+                if (tgtType == typeof(string))
+                    return (T)(object)string.Empty;
+
+                // 值类型，可空类型Nullable<>也是值类型
+                if (tgtType.IsValueType)
+                    return (T)Activator.CreateInstance(tgtType);
+
+                return default(T);
+            }
+
+            // 若指定类型和当前类型匹配
+            if (tgtType.IsAssignableFrom(val.GetType()))
+                return (T)val;
+
+            // 枚举类型
+            if (tgtType.IsEnum)
+            {
+                if (val is string str)
+                    return (str == string.Empty) ? (T)Enum.ToObject(tgtType, 0) : (T)Enum.Parse(tgtType, str);
+                return (T)Enum.ToObject(tgtType, val);
+            }
+
+            // 可空类型
+            if (tgtType.IsGenericType && tgtType.GetGenericTypeDefinition() == typeof(Nullable<>))
+            {
+                Type tp = tgtType.GetGenericArguments()[0];
+                // 参数类型不同时先转换
+                if (tp != val.GetType())
+                    return (T)Convert.ChangeType(val, tp);
+                return (T)val;
+            }
+
+            // bool特殊处理
+            if (tgtType == typeof(bool))
+            {
+                string str = val.ToString().ToLower();
+                return (T)(object)(str == "1" || str == "true");
+            }
+
+            // 执行转换
+            if (val is IConvertible)
+                return (T)Convert.ChangeType(val, tgtType);
+
+            throw new Exception($"【{p_id}】列值转换异常：无法将【{val}】转换到【{tgtType.Name}】类型！");
+        }
+        #endregion
+
+        #region 内部
         /// <summary>
         /// 承载样式的根元素，设置样式时自动创建
         /// </summary>
@@ -152,5 +258,6 @@ namespace Dt.Base
                 Root = new ContentPresenter();
             return Root;
         }
+        #endregion
     }
 }
