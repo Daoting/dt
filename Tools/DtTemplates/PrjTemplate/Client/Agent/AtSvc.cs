e1ed650994efa2eab637ca1ab6b2a5835a8c2ce8
daoting <daoting@outlook.com>
2022-12-05 15:59:08 +0800

    升级服务的引用包

--- Tools/DtTemplates/PrjTemplate/Client/Agent/AtSvc.cs
+++ Tools/DtTemplates/PrjTemplate/Client/Agent/AtSvc.cs
@@ -11,8 +11,11 @@ namespace $ext_safeprojectname$
     /// <summary>
     /// lob服务Api
     /// </summary>
-    public partial class AtSvc : DataProvider<lob>
-    { }
-
-    public class lob { }
+    public partial class AtSvc : DataProvider<AtSvc.LOB>
+    {
+        /// <summary>
+        /// 只为提供服务名称
+        /// </summary>
+        public class LOB { }
+    }
 }
