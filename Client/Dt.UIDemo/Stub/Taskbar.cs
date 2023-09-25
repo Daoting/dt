c60ef57c2cac8f2ea1b3b47b70417eefb7851830
daoting <daoting@outlook.com>
2023-09-25 11:56:15 +0800

    增加托盘demo

--- Client/Dt.UIDemo/Stub/Taskbar.cs
+++ Client/Dt.UIDemo/Stub/Taskbar.cs
@@ -8,6 +8,7 @@
 
 #region 引用命名
 using Dt.Base;
+using Dt.Base.Tools;
 using Microsoft.UI;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
@@ -44,7 +45,39 @@ namespace Dt.UIDemo
                 Content = "\uE004",
                 Style = Res.字符按钮,
             };
+            btn.Click += Btn_Click;
             return btn;
+        }
+
+        void Btn_Click(object sender, RoutedEventArgs e)
+        {
+            var nav = new NavList { ViewMode = NavViewMode.Tile, Title = "系统", To = NavTarget.NewWin };
+            nav.Data = new Nl<Nav>
+            {
+                new Nav("本地库", typeof(LocalDbWin), Icons.数据库) { Desc = "管理 LocalState\\.data 目录下的 sqlite 库" },
+                new Nav("本地文件", typeof(LocalFileWin), Icons.文件) { Desc = "管理 LocalState 的所有文件" },
+                new Nav("更新缓存文件", typeof(RefreshSqliteWin), Icons.刷新) { Desc = "刷新服务端指定的 sqlite 缓存文件" },
+
+                new Nav("数据库工具", typeof(RemoteDbWin), Icons.数据库) { Desc = "数据库初始化、备份等功能" },
+
+                new Nav("类型别名", typeof(TypeAliasWin), Icons.划卡) { Desc = "所有为类型命名别名的名称与类型的列表" },
+
+                new Nav("切换服务", null, Icons.服务器) { Desc = "切换服务地址", Callback = (s, n) =>
+                {
+                    SysTrace.ToggleSvcUrl();
+                    if (s is Dlg dlg)
+                        dlg.Close();
+                } },
+            };
+
+            var dlg = new Dlg
+            {
+                HideTitleBar = true,
+                WinPlacement = DlgPlacement.FromTopRight,
+                Width = 500,
+            };
+            dlg.LoadTab(nav);
+            dlg.Show();
         }
     }
 }
