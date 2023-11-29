9108a87048c0f82b752604bad4209fcb8991ec28
daoting <daoting@outlook.com>
2023-11-29 16:35:21 +0800

    为VS增加中文代码助手扩展，因网上未找到好用的

---
+++ Tools/DtChineseAssist/ChineseCompletionSource.cs
@@ -0,0 +1,130 @@
+﻿using Microsoft.VisualStudio.Core.Imaging;
+using Microsoft.VisualStudio.Language.Intellisense.AsyncCompletion;
+using Microsoft.VisualStudio.Language.Intellisense.AsyncCompletion.Data;
+using Microsoft.VisualStudio.Text;
+using Microsoft.VisualStudio.Text.Adornments;
+using Microsoft.VisualStudio.Text.Operations;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DtChineseAssist
+{
+    class ChineseCompletionSource : IAsyncCompletionSource
+    {
+        static ImmutableArray<CompletionFilter> _filters = ImmutableArray.Create(
+            new CompletionFilter(
+                "简拼",
+                "j",
+                new ImageElement(new ImageId(new Guid("ae27a6b0-e345-4288-96df-5eaf394ee369"), 2436), "简拼")));
+
+        readonly ITextStructureNavigatorSelectorService _textNavigator;
+        readonly IAsyncCompletionSource _sharpSource;
+
+        public ChineseCompletionSource(ITextStructureNavigatorSelectorService p_textNavigator, IAsyncCompletionSource p_sharpSource)
+        {
+            _textNavigator = p_textNavigator;
+            _sharpSource = p_sharpSource;
+        }
+
+        public CompletionStartData InitializeCompletion(CompletionTrigger trigger, SnapshotPoint triggerLocation, CancellationToken token)
+        {
+            var tokenSpan = FindTokenSpanAtPosition(triggerLocation);
+            return new CompletionStartData(CompletionParticipation.ProvidesItems, tokenSpan);
+        }
+
+        SnapshotSpan FindTokenSpanAtPosition(SnapshotPoint triggerLocation)
+        {
+            ITextStructureNavigator navigator = _textNavigator.GetTextStructureNavigator(triggerLocation.Snapshot.TextBuffer);
+            TextExtent extent = navigator.GetExtentOfWord(triggerLocation);
+            if (triggerLocation.Position > 0 && (!extent.IsSignificant || !extent.Span.GetText().Any(c => char.IsLetterOrDigit(c))))
+            {
+                extent = navigator.GetExtentOfWord(triggerLocation - 1);
+            }
+            var tokenSpan = triggerLocation.Snapshot.CreateTrackingSpan(extent.Span, SpanTrackingMode.EdgeInclusive);
+            var snapshot = triggerLocation.Snapshot;
+            var tokenText = tokenSpan.GetText(snapshot);
+            if (string.IsNullOrWhiteSpace(tokenText))
+            {
+                return new SnapshotSpan(triggerLocation, 0);
+            }
+            int startOffset = 0;
+            int endOffset = 0;
+            if (tokenText.Length > 0)
+            {
+                if (tokenText.StartsWith("\""))
+                    startOffset = 1;
+            }
+            if (tokenText.Length - startOffset > 0)
+            {
+                if (tokenText.EndsWith("\"\r\n"))
+                    endOffset = 3;
+                else if (tokenText.EndsWith("\r\n"))
+                    endOffset = 2;
+                else if (tokenText.EndsWith("\"\n"))
+                    endOffset = 2;
+                else if (tokenText.EndsWith("\n"))
+                    endOffset = 1;
+                else if (tokenText.EndsWith("\""))
+                    endOffset = 1;
+            }
+            return new SnapshotSpan(tokenSpan.GetStartPoint(snapshot) + startOffset, tokenSpan.GetEndPoint(snapshot) - endOffset);
+        }
+
+        public async Task<CompletionContext> GetCompletionContextAsync(IAsyncCompletionSession session, CompletionTrigger trigger, SnapshotPoint triggerLocation, SnapshotSpan applicableToSpan, CancellationToken token)
+        {
+            if (_sharpSource == null || Kit.RecursionLock.Value)
+                return null;
+
+            try
+            {
+                Kit.RecursionLock.Value = true;
+                CompletionContext context = await _sharpSource.GetCompletionContextAsync(session, trigger, triggerLocation, applicableToSpan, token);
+                if (context.Items.Length == 0)
+                    return null;
+
+                var items = new ConcurrentBag<CompletionItem>();
+                foreach (var ci in context.Items)
+                {
+                    // 其他命名空间的不处理，否则出现两遍相同的选项
+                    if (!string.IsNullOrEmpty(ci.Suffix))
+                        continue;
+
+                    string rawStr = ci.InsertText;
+                    var pinyin = Kit.GetPingYin(rawStr);
+                    if (pinyin != null)
+                    {
+                        var item = new CompletionItem(
+                                displayText: rawStr,
+                                source: this,
+                                icon: ci.Icon,
+                                filters: _filters,
+                                // 不加[]会显示两遍名称
+                                suffix: $"[{pinyin}]",
+                                insertText: rawStr,
+                                sortText: pinyin,
+                                filterText: pinyin,
+                                automationText: ci.AutomationText,
+                                attributeIcons: ci.AttributeIcons);
+                        items.Add(item);
+                    }
+                }
+
+                //Debug.WriteLine($"触发：{trigger.Reason}；共：{context.Items.Length}；中文：{items.Count}\r\n");
+                return new CompletionContext(items.ToImmutableArray());
+            }
+            finally
+            {
+                Kit.RecursionLock.Value = false;
+            }
+        }
+
+        public Task<object> GetDescriptionAsync(IAsyncCompletionSession session, CompletionItem item, CancellationToken token)
+        {
+            return Task.FromResult((object)null);
+        }
+    }
+}
---
+++ Tools/DtChineseAssist/ChineseCompletionSourceProvider.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.VisualStudio.Language.Intellisense.AsyncCompletion;
+using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.Text.Operations;
+using Microsoft.VisualStudio.Utilities;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+
+namespace DtChineseAssist
+{
+    [Export(typeof(IAsyncCompletionSourceProvider))]
+    [Name("中文代码助手")]
+    [ContentType("CSharp")]
+    internal class ChineseCompletionSourceProvider : IAsyncCompletionSourceProvider
+    {
+        IDictionary<ITextView, IAsyncCompletionSource> _cache = new Dictionary<ITextView, IAsyncCompletionSource>();
+
+        [Import]
+        ITextStructureNavigatorSelectorService _textNavigator;
+
+        [ImportMany]
+        Lazy<IAsyncCompletionSourceProvider>[] _lazyProviders;
+
+        [Import]
+        IAsyncCompletionBroker _broker;
+
+        public IAsyncCompletionSource GetOrCreate(ITextView textView)
+        {
+            if (_cache.TryGetValue(textView, out var itemSource))
+                return itemSource;
+
+            IAsyncCompletionSource sharpSource = GetSharpSource(textView);
+            var source = new ChineseCompletionSource(_textNavigator, sharpSource);
+            textView.Closed += (o, e) => _cache.Remove(textView);
+            _cache.Add(textView, source);
+            return source;
+        }
+
+        IAsyncCompletionSource GetSharpSource(ITextView textView)
+        {
+            if (_lazyProviders != null && _lazyProviders.Length > 0)
+            {
+                for (int i = 0; i < _lazyProviders.Length; i++)
+                {
+                    var provider = _lazyProviders[i];
+                    if (provider.IsValueCreated)
+                    {
+                        Type type = provider.Value.GetType();
+                        if (type.FullName == _providerType)
+                        {
+                            return provider.Value.GetOrCreate(textView);
+                        }
+                    }
+                }
+            }
+            
+            if (_broker != null)
+            {
+                var prvs = _broker.GetType().GetField("orderedCompletionSourceProviders", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
+                if (prvs != null)
+                {
+                    IList list = prvs.GetValue(_broker) as IList;
+                    if (list != null)
+                    {
+                        for (int i = 0; i < list.Count; i++)
+                        {
+                            Lazy<IAsyncCompletionSourceProvider> provider = list[i] as Lazy<IAsyncCompletionSourceProvider>;
+                            if (provider.IsValueCreated)
+                            {
+                                Type type = provider.Value.GetType();
+                                if (type.FullName == _providerType)
+                                {
+                                    return provider.Value.GetOrCreate(textView);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            return null;
+        }
+
+        const string _providerType = "Microsoft.CodeAnalysis.Editor.Implementation.IntelliSense.AsyncCompletion.CompletionSourceProvider";
+    }
+}
