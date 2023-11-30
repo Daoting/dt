a94271c08d0568b251de6c2f4af7101801ac3088
daoting <daoting@outlook.com>
2023-11-30 13:46:06 +0800

    Tools移到独立库中

--- Tools/DtTemplates/PrjItemTemplate/Readme.txt
+++
@@ -1,4 +0,0 @@
-﻿
-请在 RefAll.props 中添加引用
-
-<ProjectReference Include="..\$safeprojectname$\$safeprojectname$.csproj" />
--- Tools/DtTemplates/PrjTemplate/Wasm/WasmScripts/AppManifest.js
+++
@@ -1,5 +0,0 @@
-﻿var UnoAppManifest = {
-    splashScreenImage: "assets/SplashScreen.png",
-    splashScreenColor: "#1BA1E2",
-    displayName: "$ext_safeprojectname$"
-}
