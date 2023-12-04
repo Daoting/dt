7bc92fbf70b02b2e4605a65bf639f979c898ea25
daoting <daoting@outlook.com>
2023-12-04 16:01:13 +0800

    dt.infras独立库

--- Infras/Dt.Cells.Data/Data/StickyNote.cs
+++
@@ -1,7 +0,0 @@
-namespace Dt.Cells.Data
-{
-    internal class StickyNote : ShapeBase
-    {
-    }
-}
-
--- Infras/Dt.Charts/Chart/IMeasureSymbolWidth.cs
+++
@@ -1,7 +0,0 @@
-namespace Dt.Charts
-{
-    internal interface IMeasureSymbolWidth
-    {
-    }
-}
-
