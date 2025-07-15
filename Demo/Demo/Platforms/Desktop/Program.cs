6dd3ebd92ff01e41e8a1855a7f2514c34c812128
dao'ting <daoting@outlook.com>
2025-07-15 16:04:24 +0800

    STAThread

--- Demo/Demo/Platforms/Desktop/Program.cs
+++ Demo/Demo/Platforms/Desktop/Program.cs
@@ -1,12 +1,19 @@
 using Uno.UI.Hosting;
 using Demo;
 
-var host = UnoPlatformHostBuilder.Create()
-    .App(() => new App())
-    .UseX11()
-    .UseLinuxFrameBuffer()
-    .UseMacOS()
-    .UseWin32()
-    .Build();
+internal class Program
+{
+    [STAThread]
+    public static void Main(string[] args)
+    {
+        var host = UnoPlatformHostBuilder.Create()
+            .App(() => new App())
+            .UseX11()
+            .UseLinuxFrameBuffer()
+            .UseMacOS()
+            .UseWin32()
+            .Build();
 
-host.Run();
+        host.Run();
+    }
+}
