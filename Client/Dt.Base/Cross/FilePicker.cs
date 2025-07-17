fc3074f9cfe0e9fb7c3cfcaf54d6ce5ae6efcff9
dao'ting <daoting@outlook.com>
2025-07-17 15:10:01 +0800

    android版FilePicker的bug

--- Client/Dt.Base/Cross/FilePicker.cs
+++ Client/Dt.Base/Cross/FilePicker.cs
@@ -1,4 +1,4 @@
-﻿#if WIN || WASM || DESKTOP
+﻿#if WIN || WASM || DESKTOP || ANDROID
 #region 文件描述
 /******************************************************************************
 * 创建: Daoting
@@ -155,18 +155,34 @@ namespace Dt.Base
             return ls;
         }
 
+        static FileOpenPicker CreatePicker(PickerLocationId p_locationstring, string[] p_allowedTypes)
+        {
+            var picker = Kit.GetFileOpenPicker();
+            picker.ViewMode = PickerViewMode.List;
+            picker.SuggestedStartLocation = p_locationstring;
+
+            var hasAtleastOneType = false;
+            if (p_allowedTypes != null)
+            {
+                foreach (var type in p_allowedTypes)
+                {
+                    if (type.StartsWith("."))
+                    {
+                        picker.FileTypeFilter.Add(type);
+                        hasAtleastOneType = true;
+                    }
+                }
+            }
+            if (!hasAtleastOneType)
+                picker.FileTypeFilter.Add("*");
+            return picker;
+        }
+
+#if WIN    
         static async Task<FileData> GetFileData(StorageFile p_file)
         {
-            FileData fd;
-#if WIN
             string id = StorageApplicationPermissions.FutureAccessList.Add(p_file);
-            fd = new FileData(id, p_file.Name, (await p_file.GetBasicPropertiesAsync()).Size);
-#elif WASM
-            fd = new FileData(p_file.Name, p_file.Name, (await p_file.GetBasicPropertiesAsync()).Size);
-            fd.FileStream = await p_file.OpenStreamForReadAsync();
-#else
-            fd = new FileData(p_file.Path, p_file.Name, (await p_file.GetBasicPropertiesAsync()).Size);
-#endif
+            var fd = new FileData(id, p_file.Name, (await p_file.GetBasicPropertiesAsync()).Size);
             string ext = fd.Ext;
             bool existThumb = false;
 
@@ -202,29 +218,93 @@ namespace Dt.Base
             }
             return fd;
         }
-
-        static FileOpenPicker CreatePicker(PickerLocationId p_locationstring, string[] p_allowedTypes)
+        
+#elif ANDROID
+        static async Task<FileData> GetFileData(StorageFile p_file)
         {
-            var picker = Kit.GetFileOpenPicker();
-            picker.ViewMode = PickerViewMode.List;
-            picker.SuggestedStartLocation = p_locationstring;
+            Android.Net.Uri uri = Android.Net.Uri.Parse(p_file.Path);
+            var filePath = IOUtil.GetPath(Android.App.Application.Context, uri);
+            if (string.IsNullOrEmpty(filePath))
+                filePath = IOUtil.IsMediaStore(uri.Scheme) ? uri.ToString() : uri.Path;
+            FileData fd = new FileData(filePath, p_file.Name, (await p_file.GetBasicPropertiesAsync()).Size);
+            string ext = fd.Ext;
 
-            var hasAtleastOneType = false;
-            if (p_allowedTypes != null)
+            // 详细描述
+            if (FileFilter.UwpImage.Contains(ext))
             {
-                foreach (var type in p_allowedTypes)
+                Android.Graphics.BitmapFactory.Options options = new Android.Graphics.BitmapFactory.Options();
+                // 只解析图片大小，不加载内容
+                options.InJustDecodeBounds = true;
+                Android.Graphics.BitmapFactory.DecodeFile(filePath, options);
+                fd.Desc = $"{options.OutWidth} x {options.OutHeight} ({ext.TrimStart('.')})";
+
+                int maxSize = Math.Max(options.OutWidth, options.OutHeight);
+                if (maxSize > FileData.ThumbSize)
                 {
-                    if (type.StartsWith("."))
+                    // 直接按缩放比例加载
+                    options.InJustDecodeBounds = false;
+                    options.InSampleSize = maxSize / FileData.ThumbSize;
+                    // v29 弃用
+                    //options.InPurgeable = true;
+                    Android.Graphics.Bitmap bmp = Android.Graphics.BitmapFactory.DecodeFile(filePath, options);
+
+                    fd.ThumbPath = System.IO.Path.Combine(Kit.CachePath, Kit.NewGuid + "-t.jpg");
+                    using (var fs = System.IO.File.Create(fd.ThumbPath))
                     {
-                        picker.FileTypeFilter.Add(type);
-                        hasAtleastOneType = true;
+                        await bmp.CompressAsync(Android.Graphics.Bitmap.CompressFormat.Jpeg, 100, fs);
                     }
+                    bmp.Recycle();
                 }
             }
-            if (!hasAtleastOneType)
-                picker.FileTypeFilter.Add("*");
-            return picker;
+            else if (FileFilter.UwpVideo.Contains(ext))
+            {
+                Android.Media.MediaMetadataRetriever media = new Android.Media.MediaMetadataRetriever();
+                try
+                {
+                    await media.SetDataSourceAsync(filePath);
+                    string dur = media.ExtractMetadata(Android.Media.MetadataKey.Duration);
+                    string width = media.ExtractMetadata(Android.Media.MetadataKey.VideoWidth);
+                    string height = media.ExtractMetadata(Android.Media.MetadataKey.VideoHeight);
+                    fd.Desc = string.Format("{0:HH:mm:ss} ({1} x {2})", new DateTime(long.Parse(dur) * 10000), width, height);
+                }
+                catch { }
+                finally
+                {
+                    media.Release();
+                }
+
+                // 帧缩略图
+                var bmp = await Android.Media.ThumbnailUtils.CreateVideoThumbnailAsync(filePath, Android.Provider.ThumbnailKind.MiniKind);
+                fd.ThumbPath = System.IO.Path.Combine(Kit.CachePath, Kit.NewGuid + "-t.jpg");
+                using (var fs = System.IO.File.Create(fd.ThumbPath))
+                {
+                    await bmp.CompressAsync(Android.Graphics.Bitmap.CompressFormat.Jpeg, 100, fs);
+                }
+                bmp.Recycle();
+            }
+            else if (FileFilter.UwpAudio.Contains(ext))
+            {
+                Android.Media.MediaMetadataRetriever media = new Android.Media.MediaMetadataRetriever();
+                try
+                {
+                    await media.SetDataSourceAsync(filePath);
+                    string dur = media.ExtractMetadata(Android.Media.MetadataKey.Duration);
+                    fd.Desc = string.Format("{0:mm:ss}", new DateTime(long.Parse(dur) * 10000));
+                }
+                catch { }
+                finally
+                {
+                    media.Release();
+                }
+            }
+            return fd;
         }
+
+#elif WASM
+
+#elif IOS
+
+#endif
         #endregion
     }
 }
